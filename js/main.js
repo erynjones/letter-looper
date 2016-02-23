a161d7f04c6a61e0fca79b0027f2966354d66d0a
erynjones <eryn.jones16@hotmail.com>
2016-02-23 17:50:51 -0500

    fixes for markbot

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-var letter = prompt ('Enter a Letter');
+var letter = prompt('Enter a Letter');
 
-for (i = 0; i <10; i++) {
+for (i = 0; i < 10; i++) {
   document.write('a');
   document.write('<br>');
 }
